2fe57e568b1afac1ee99e291d3ebca6a40a932a3
ambuila106 <brandon.ambuila@unillanos.edu.co>
2020-11-09 13:02:44 -0500

    added push notifications methods to app.vue, apply plugin com.google.gms.google-services

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,59 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+} from "@capacitor/core";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
-    IonRouterOutlet
-  }
+    IonRouterOutlet,
+  },
+  methods: {},
+  setup() {
+    const { PushNotifications } = Plugins;
+
+    PushNotifications.requestPermission().then((result) => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener(
+      "registration",
+      (token: PushNotificationToken) => {
+        alert("Push registration success, token: " + token.value);
+      }
+    );
+
+    PushNotifications.addListener("registrationError", (error: any) => {
+      alert("Error on registration: " + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      "pushNotificationReceived",
+      (notification: PushNotification) => {
+        alert("Push received: " + JSON.stringify(notification));
+      }
+    );
+
+    PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification: PushNotificationActionPerformed) => {
+        alert("Push action performed: " + JSON.stringify(notification));
+      }
+    );
+  },
 });
 </script>
